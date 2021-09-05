109908abf229497de8caf2523cbb7a0ab33f55ef
JekasObps <jekas.obps@gmail.com>
2021-09-05 17:56:40 +0300

    thread pool changed from async to pair of future and promise which give more control over execution.

--- thread_pool.hpp
+++ thread_pool.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
+#include <vector>
 #include <thread>
 #include <future>
 #include <functional>
-#include <vector>
 
 #include <mutex>
 #include "spinlock.hpp"
@@ -12,20 +12,17 @@ namespace obps
 {
 
 template <
-    typename ThreadRet, // user defined status enum that pool will use to indicate a state of thread execution  
-    typename E,         // Exception Base that will be catched
-    ThreadRet completed,// !value template parameter. Status that stops completed task thread.
-    ThreadRet critical  // status that indicates criticalness of the error in user task (can the task continue after catching this exception?)
+    typename ThreadRet, // user defined status enum that pool will use to indicate a state of thread execution
+    ThreadRet running,  // continue looping on thread_func
+    ThreadRet finished, // stop looping, task completed
+    ThreadRet aborted   // status that indicates errorneus execution stop
 > class ThreadPool final 
 {
 public:
-    enum class State_ {RUNNING, SHUTDOWN};
-
-    using OnExcept = std::function<ThreadRet( const E& )>; 
+    enum class PoolState {RUNNING, SHUTDOWN};
 
     template <typename ...Args> 
     void RunTask(
-        OnExcept on_except,                             // checks if exception is critical
         std::function<ThreadRet(Args...)> thread_func,  // user task
         Args ...args                                    // user arguments to pass into the task
     ) noexcept;
@@ -48,68 +45,89 @@ public:
         ShutDown();
     }
 private:
-    ThreadPool() : m_State{State_::RUNNING} 
+    ThreadPool() : m_State{PoolState::RUNNING} 
     {}
     
     static std::shared_ptr<ThreadPool> s_Instance;
     
-    State_ m_State; 
-    std::vector<std::future<ThreadRet>> m_Tasks;
-    spinlock m_PoolLock;    // prevents racecondition when running tasks and on shutdown
+    PoolState m_State;
+
+    std::vector<std::thread> m_Threads;
+    std::vector<std::pair<std::promise<ThreadRet>, std::future<ThreadRet>>> m_Tasks;
+    spinlock m_PoolLock;    // prevents race-condition when running tasks and on shutdown
 };
 
-template <typename ThreadRet, typename E, ThreadRet completed, ThreadRet critical>
+template <typename ThreadRet, ThreadRet running, ThreadRet finished, ThreadRet aborted>
 template <typename ...Args> 
-void ThreadPool<ThreadRet, E, completed, critical>::RunTask( 
-    OnExcept on_except, std::function<ThreadRet(Args...)> thread_func, Args ...args) noexcept 
+void ThreadPool<ThreadRet, running, finished, aborted>::RunTask(
+    std::function<ThreadRet(Args...)> thread_func, Args ...args) noexcept 
 {
-    std::lock_guard<spinlock> _lock(m_PoolLock);
-    m_Tasks.emplace_back(
-        std::async([this, on_except, thread_func, args...] {
-            while(m_State == State_::RUNNING)
-            {
-            try
+    std::lock_guard<spinlock> lock(m_PoolLock);
+    
+    auto& [promise, future] = m_Tasks.emplace_back();
+    future = promise.get_future();
+
+    m_Threads.emplace_back([this, thread_func, &promise, args...] {
+        while(m_State == PoolState::RUNNING)
+        {
+            try 
             {
                 ThreadRet status = thread_func(args...);
-                if (status == completed || status == critical)
+                switch (status)
                 {
-                    return status;
+                    case running: 
+                        continue; break;
+
+                    case finished:
+                    case aborted:
+                    {
+                        promise.set_value_at_thread_exit(status);
+                        return;
+                    }
+
+                    default: 
+                        continue;
                 }
             }
-            catch(const E& e)
+            catch(const std::exception& e)
             {
-                if (on_except(e) == critical)
+                try 
                 {
-                    return critical;
+                    promise.set_exception(std::current_exception());
+                } 
+                catch(const std::exception& e) {
+                    std::cerr << "Exception was thrown on promise::set_exception:\n"
+                              << e.what() << std::endl;
                 }
+
+                return;
             }
-            }
-            return critical;
-        })
-    );
+        }
+    });
 }
 
 
-template <typename ThreadRet, typename E, ThreadRet completed, ThreadRet critical>
-void ThreadPool<ThreadRet, E, completed, critical>::ShutDown() noexcept
+template <typename ThreadRet, ThreadRet running, ThreadRet completed, ThreadRet aborted>
+void ThreadPool<ThreadRet, running, completed, aborted>::ShutDown() noexcept
 {
-    std::lock_guard<spinlock> _lock(m_PoolLock);
+    std::lock_guard<spinlock> lock(m_PoolLock);
     // state propagates to all threads
-    m_State = State_::SHUTDOWN;
-
-    while((! m_Tasks.empty()))
+    m_State = PoolState::SHUTDOWN;
+    
+    while((!m_Threads.empty()))
     {
-        auto && task = m_Tasks.back();
-        ThreadRet result = task.get();
+        m_Threads.back().join();
+        ThreadRet result = m_Tasks.back().second.get();
+        m_Threads.pop_back();
         m_Tasks.pop_back();
-        // do something with result
-        // ?? maybe by passing to a user hook ?? 
+
+        // TODO: process result
     }
 }
 
 
 // ! instantiation of a ThreadPool singleton !
-template <typename ThreadRet, typename E, ThreadRet completed, ThreadRet critical>
-std::shared_ptr<ThreadPool<ThreadRet, E, completed, critical>> ThreadPool<ThreadRet, E, completed, critical>::s_Instance;
+template <typename ThreadRet, ThreadRet running, ThreadRet completed, ThreadRet aborted>
+inline std::shared_ptr<ThreadPool<ThreadRet, running, completed, aborted>> ThreadPool<ThreadRet, running, completed, aborted>::s_Instance;
 
 } // namespace obps
