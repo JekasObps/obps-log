c1c3b38303e01730defdb308236a6809bfbdd85f
JekasObps <jekas.obps@gmail.com>
2021-09-05 17:58:25 +0300

    trying to catch bug when not all messages get printed before shutdown

--- simple_test.cpp
+++ simple_test.cpp
@@ -1,10 +1,11 @@
 #include "obps_log_public.hpp"
 
+// GLOBAL_LOG(ERROR, std::cerr);
 
 // simple test to check enabling and disabling logging 
 void test1()
 {
-    SCOPE_LOG(INFO, std::cout);
+    SCOPE_LOG(INFO, std::cerr);
 
     USER_LEVEL("---"); // will not get logged
     INFO("Pass");      // ok
@@ -12,7 +13,13 @@ void test1()
 
 int main()
 {
+    SCOPE_LOG(INFO, std::cout);
+    OBPS_LOG_INIT();
+    INFO("Cass");      // ok
     test1();
+    test1();
+    test1();
+    test1();
+    INFO("Cass");      // ok
 
-    OBPS_LOG_DESTROY();
 }
