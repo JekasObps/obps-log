57550bd11fb3ebf46b4b373bfee815a853649eae
JekasObps <jekas.obps@gmail.com>
2021-09-21 11:12:35 +0300

    + macros + constructiong obps log with assign operator (allows it to be used as a test class member....

--- src/obps_log_public.hpp
+++ src/obps_log_public.hpp
@@ -29,9 +29,12 @@
     *   Create log in global scope.
     */
     #define GLOBAL_LOG(...) \
-        obps::Log _GLOBAL_LOG_ID({__VA_ARGS__}); \
+        obps::Log _GLOBAL_LOG_ID = obps::Log({__VA_ARGS__}); \
         obps::Log& get_global_log(){ return _GLOBAL_LOG_ID; } \
         // getter helps to ensure that a global log has been initialized on cross translation unit access 
+
+    #define G_MUTE(...) get_global_log().Mute({__VA_ARGS__})
+    #define G_UNMUTE(...) get_global_log().Unmute({__VA_ARGS__})
 
     /*
     *   Call at the beginning of the logging scope
@@ -39,8 +42,16 @@
     #define SCOPE_LOG(...) \
         static obps::Log _SCOPE_LOG_ID({__VA_ARGS__})
 
+    #define MUTE(...) _SCOPE_LOG_ID.Mute({__VA_ARGS__})
+    #define UNMUTE(...) _SCOPE_LOG_ID.Unmute({__VA_ARGS__})
 #else
-    #define OBPS_LOG_TEARDOWN() ;
-    #define GLOBAL_LOG(...) ; 
-    #define SCOPE_LOG(...) ;
+    #define OBPS_LOG_TEARDOWN() {}
+    #define GLOBAL_LOG(...)
+    #define SCOPE_LOG(...)
+
+    #define MUTE(...) {}
+    #define G_MUTE(...) {}
+    #define UNMUTE(...) {}
+    #define G_UNMUTE(...) {}
+
 #endif // LOG_ON
